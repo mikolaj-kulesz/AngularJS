21c89b56983b5c7f2ffce8832e8819e4bea895ff
Your Name <you@example.com>
2017-02-02 19:05:21 +0100

    v.11.custom-directive

--- 11/directive-with-template/directive.js
+++ 11/directive-with-template/directive.js
@@ -1,7 +1,20 @@
-// Plik: r11/directive-with-template/directive.js
 angular.module('stockMarketApp')
-  .directive('stockWidget', [function() {
-    return {
-      templateUrl: 'stock.html'
-    };
-  }]);
+.directive('stockWidget', [function(){
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		templateUrl: 'stock.html',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		//link: function($scope, iElm, iAttrs, controller) {
+			
+		//}
+	};
+}]);
