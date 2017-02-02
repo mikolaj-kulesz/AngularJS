f51a27d8cefa381fab82ac62745a576136eba15d
Your Name <you@example.com>
2017-02-02 14:35:15 +0100

    v.10.full-example-ng-route-and-services

--- 10/routing-example/app/scripts/controllers.js
+++ 10/routing-example/app/scripts/controllers.js
@@ -1,50 +1,52 @@
-// Plik: r10/routing-example/app/scripts/controllers.js
 angular.module('fifaApp')
-  .controller('MainCtrl', ['UserService',
-    function(UserService) {
-      var self = this;
-      self.userService = UserService;
-
-      // sprawdzenie czy użytkownik jest zalogowany podczas
-      // ładowania aplikacji
-      // Usługa UserService automatycznie zaktualizuje isLoggedIn
-      // po zakończeniu tego wywołania
-      UserService.session();
+  .controller('MainCtrl', ['UserService', function(UserService){
+    var self = this;
+    self.userService = UserService;
+    // sprawdzenie czy użytkownik jest zalogowany podczas
+    // ładowania aplikacji
+    // Usługa UserService automatycznie zaktualizuje isLoggedIn
+    // po zakończeniu tego wywołania
+    
+    UserService.session();
   }])
+  .controller('TeamListCtrl', ['FifaService', function(FifaService){
+    var self = this;
+    self.teams = [];
 
-  .controller('TeamListCtrl', ['FifaService',
-    function(FifaService) {
-      var self = this;
-      self.teams = [];
-
-      FifaService.getTeams().then(function(resp) {
-        self.teams = resp.data;
-      });
+    FifaService.getTeams()
+      .then(
+        function(response){
+          self.teams = response.data;
+        }
+      )
   }])
+  .controller('TeamDetailsCtrl', ['$location', '$routeParams', 'FifaService', function($location, $routeParams, FifaService){
+    var self = this;
+    self.team = {};
 
-  .controller('LoginCtrl', ['UserService', '$location',
-    function(UserService, $location) {
-      var self = this;
-      self.user = {username: '', password: ''};
+    FifaService.getTeamDetails($routeParams.code)
+      .then(
+        function(response){
+          self.team = response.data;
+        },
+        function(error){
+          $location.path('/login');
+        }
+      )
+  }])
+  .controller('LoginCtrl', ['UserService', '$location', function(UserService, $location){
+    var self = this;
+    self.user = {username: '', password: ''};
 
-      self.login = function() {
-        UserService.login(self.user).then(function(success) {
+    self.login = function(){
+      UserService.login(self.user)
+      .then(
+        function(success){
           $location.path('/');
-        }, function(error) {
+        },
+        function(error){
           self.errorMessage = error.data.msg;
-        })
-      };
-  }])
-
-  .controller('TeamDetailsCtrl',
-    ['$location', '$routeParams', 'FifaService',
-    function($location, $routeParams, FifaService) {
-      var self = this;
-      self.team = {};
-      FifaService.getTeamDetails($routeParams.code)
-          .then(function(resp){
-        self.team = resp.data;
-      }, function(error){
-        $location.path('/login');
-      });
-    }]);
+        }
+      )
+    };
+  }]);
