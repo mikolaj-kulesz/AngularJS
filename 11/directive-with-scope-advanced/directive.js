42f9f095def4f1a0536344e5931e2e1a3973e046
Your Name <you@example.com>
2017-02-03 13:14:44 +0100

    v.11.directive-with-scope-advanced-example

--- 11/directive-with-scope-advanced/directive.js
+++ 11/directive-with-scope-advanced/directive.js
@@ -1,26 +1,32 @@
-// Plik: r11/directive-with-scope-advanced/directive.js
 angular.module('stockMarketApp')
   .directive('stockWidget', [function() {
     return {
       templateUrl: 'stock.html',
       restrict: 'A',
       scope: {
-        stockData: '=',
+        stockDataStocks: '=',
         stockTitle: '@',
         whenSelect: '&'
       },
       link: function($scope, $element, $attrs) {
         $scope.getChange = function(stock) {
-          return Math.ceil(((stock.price - stock.previous) /
-              stock.previous) * 100);
+          return Math.ceil(((stock.price - stock.previous) / stock.previous) * 100);
         };
 
-        $scope.onSelect = function() {
+/*        $scope.onSelect = function() {
           $scope.whenSelect({
-            stockName: $scope.stockData.name,
-            stockPrice: $scope.stockData.price,
-            stockPrevious: $scope.stockData.previous
-          });
+            stockName: $scope.stockDataStocks.name,
+            stockPrice: $scope.stockDataStocks.price,
+            stockPrevious: $scope.stockDataStocks.previous
+          })
+        };*/
+
+        $scope.onSelect = function(stock) {
+          $scope.whenSelect({
+            stockName: stock.name,
+            stockPrice: stock.price,
+            stockPrevious: stock.previous
+          })
         };
       }
     };
